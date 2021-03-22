e5f5f6528fd308302c552f80c4d23156b9049bfd
jyounger <81150504+jyounger@users.noreply.github.com>
2021-03-22 04:20:53 -0400

    Update plsql1.sql

--- plsql1.sql
+++ plsql1.sql
@@ -1,12 +1,44 @@
-drop sequence s1;
+// create database and tables
 
-create sequence s1
-start with 1000
-increment by 1
-nocycle
-nocache
-/
+CREATE DATABASE students_7;
 
+USE students_7;
+
+CREATE TABLE logins (
+	
+user_login VARCHAR(15) NOT NULL,
+user_password CHAR(30) NOT NULL,
+PRIMARY KEY (user_login)
+);
+
+CREATE TABLE roles (
+
+user_login VARCHAR(15) NOT NULL,
+role_name VARCHAR(15) NOT NULL,
+PRIMARY KEY (user_login, role_name)
+);
+
+CREATE TABLE student (
+
+student_id INT(11) NOT NULL AUTO_INCREMENT,
+student_name VARCHAR(60),
+student_email VARCHAR(50),
+program_name enum('Physics', 'Mathematics', 'Chemistry'),
+student_year INT(10),
+student_share TINYINT(1),
+PRIMARY KEY (student_id)
+);
+
+
+// insert values
+
+INSERT INTO roles VALUES ('djeter', 'teacher');
+INSERT INTO roles VALUES (ljames', 'teacher');
+INSERT INTO roles VALUES ('jhutter', 'student');
+
+INSERT INTO logins VALUES ('djeterr', 'yankees');
+INSERT INTO logins VALUES ('ljames', 'yankees');
+INSERT INTO logins VALUES ('arod', 'yankees');
 
 
 
